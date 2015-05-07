cf57bbec950e9ea57e5fc771b4e5a70daed9fa7a
Sindri55 <sindri12@ru.is>
2015-05-07 11:09:28 +0000

    blablalalala

--- sample/src/main/java/com/github/devnied/emvnfccard/dialog/LoginDialogFragment.java
+++ sample/src/main/java/com/github/devnied/emvnfccard/dialog/LoginDialogFragment.java
@@ -40,11 +40,9 @@ public class LoginDialogFragment extends DialogFragment {
         return builder.create();
     } */
 
-
-
     /* The activity that creates an instance of this dialog fragment must
-        * implement this interface in order to receive event callbacks.
-        * Each method passes the DialogFragment in case the host needs to query it. */
+    * implement this interface in order to receive event callbacks.
+    * Each method passes the DialogFragment in case the host needs to query it. */
     public interface LoginDialogListener {
         public void onDialogPositiveClick(DialogFragment dialog);
         public void onDialogNegativeClick(DialogFragment dialog);
@@ -76,6 +74,7 @@ public class LoginDialogFragment extends DialogFragment {
         // Get the layout inflater
         LayoutInflater inflater = getActivity().getLayoutInflater();
 
+        getDialog().setTitle("Innskráning");
 
         // Inflate and set the layout for the dialog
         // Pass null as the parent view because its going in the dialog layout
